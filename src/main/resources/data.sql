cef87e195c03432c3f27965f7eec81063d89fa3f
FreemanDM <dmitry.bidenko@gmail.com>
2020-01-19 09:51:31 +0200

    Repository for "Client" entity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
-insert into client values(1, 'Vasija', 'Pupkin', 'pupkin@mail.com');
-insert into client values(2, 'Petija', 'Pijatochkin', 'pijatochkin@mail.com');
-insert into client values(3, 'Vita', 'Ivanova', 'ivanova@mail.com');
+insert into client (id, first_name, last_name, email)
+    values(1, 'Vasija', 'Pupkin', 'pupkin@mail.com');
+insert into client (id, first_name, last_name, email)
+    values(2, 'Petija', 'Pijatochkin', 'pijatochkin@mail.com');
+insert into client (id, first_name, last_name, email)
+    values(3, 'Vita', 'Ivanova', 'ivanova@mail.com');
